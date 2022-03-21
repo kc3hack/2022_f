209d5e3ea66e7201c12004477a67899380f5e7f9
Shu-Nogami <g2053930@cc.kyoto-su.ac.jp>
2022-03-21 21:12:32 +0900

    create phoneUI

---
+++ Assets/nogami/sumahoUI/scripts/ClickPhoneAppButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickPhoneAppButton : MonoBehaviour
+{
+    public void OnClick(){
+        Debug.Log("This button clicked!");
+    }
+}
