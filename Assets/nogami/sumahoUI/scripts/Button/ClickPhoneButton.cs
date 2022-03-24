8d50d3619af2616bd3247f8d8037665007122487
Shu-Nogami <g2053930@cc.kyoto-su.ac.jp>
2022-03-25 00:37:34 +0900

    sumahoUI interact calendar

--- Assets/nogami/sumahoUI/scripts/Button/ClickPhoneButton.cs
+++ Assets/nogami/sumahoUI/scripts/Button/ClickPhoneButton.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 public class ClickPhoneButton : MonoBehaviour
 {
     [SerializeField] private Canvas phoneScreenCanvas;
+    [SerializeField] private FriendMessageController fMessageController;
     public void OnClick(){
+        fMessageController.SetupFriendValue();
         this.phoneScreenCanvas.gameObject.SetActive(true);
     }
 }
--- Assets/nogami/sumahoUI/scripts/Friend/FriendMessageController.cs
+++ Assets/nogami/sumahoUI/scripts/Friend/FriendMessageController.cs
@@ -6,7 +6,10 @@ using UnityEngine.UI;
 public class FriendMessageController : MonoBehaviour
 {
     [SerializeField] private List<Button> messageList = new List<Button>();
-    private FriendStatusValue[] friendValue = new FriendStatusValue[3];
+    private List<FriendStatusValue> friendValue = new List<FriendStatusValue>();
+    [SerializeField] private List<Text> fName = new List<Text>();
+    [SerializeField] private List<Text> fMessage = new List<Text>();
+    [SerializeField] private Calendar calendar;
     private int todayFriendCount = 0;
     private void Awake(){
         this.FriendInitialize();
@@ -17,11 +20,16 @@ public class FriendMessageController : MonoBehaviour
         }
         todayFriendCount = 0;
     }
-    public void SetFriendValue(string name, string message){
-        friendValue[todayFriendCount].SetName(name);
-        friendValue[todayFriendCount].SetMessage(message);
-        messageList[todayFriendCount].gameObject.SetActive(true);
-        todayFriendCount++;
+    public void SetupFriendValue(){
+        Debug.Log("1");
+        friendValue.Clear();
+        friendValue.AddRange(calendar.getSumahoEvent());
+        todayFriendCount = friendValue.Count;
+        for(int i = 0;i < todayFriendCount;i++){
+            messageList[i].gameObject.SetActive(true);
+            fName[i].text = friendValue[i].GetName();
+            fMessage[i].text = friendValue[i].GetMessage();
+        }
     }
     public FriendStatusValue GetFriendValue(int messageNum){
         return friendValue[messageNum];
