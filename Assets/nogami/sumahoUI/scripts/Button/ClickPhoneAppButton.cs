e9fd09e09801b521f7187f1ef8acbaf78c9d6df6
Shu-Nogami <g2053930@cc.kyoto-su.ac.jp>
2022-03-25 18:44:26 +0900

    add tatibana

--- Assets/nogami/sumahoUI/scripts/Button/ClickPhoneAppButton.cs
+++ Assets/nogami/sumahoUI/scripts/Button/ClickPhoneAppButton.cs
@@ -7,8 +7,17 @@ public class ClickPhoneAppButton : MonoBehaviour
 {
     [SerializeField] private Canvas appCanvas;
     [SerializeField] private Canvas thisCanvas;
+    [SerializeField] private Calendar calenderManager;
+    [SerializeField] private GameObject jkIcon;
+    [SerializeField] private GameObject calender;
+    [SerializeField] private CalenderUI calenderUI;
+    [SerializeField] private bool isCalendar;
     public void OnClick(){
         appCanvas.gameObject.SetActive(true);
         thisCanvas.gameObject.SetActive(false);
+        if(isCalendar == true){
+            calenderUI.MoveIcon(calenderManager.getDay());
+            calenderUI.setEventIconButton(calenderManager.getClandarEvent());
+        }
     }
 }
