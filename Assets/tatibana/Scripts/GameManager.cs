5c6a64f7912266e40c663fd0984309bf6e4e9709
tachi-yuki <yukiyukididi@yahoo.co.jp>
2022-03-25 17:40:10 +0900

    edit jkModel

--- Assets/tatibana/Scripts/GameManager.cs
+++ Assets/tatibana/Scripts/GameManager.cs
@@ -1,50 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+public static class GameManager
 {
     //日にち
-    [SerializeField] private  int day;
+    [SerializeField] private static int day;
 
     //イベント情報
-    [SerializeField] private List<JkEvent> eventList = new List<JkEvent>();
+    [SerializeField] private static List<JkEvent> eventList = new List<JkEvent>();
 
     //主人公のパラメータ
-    [SerializeField] private JkModel jk;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private static JkModel jk;
 
     //日にちのゲッター
-    public int getDay()
+    public static int getDay()
     {
         return day;
     }
 
     //イベント情報のゲッター
-    public List<JkEvent> getEventList()
+    public static List<JkEvent> getEventList()
     {
         return eventList;
     }
 
     //イベント情報のゲッター
-    public JkEvent getEvent(int eventNumber)
+    public static JkEvent getEvent(int eventNumber)
     {
         return eventList[eventNumber];
     }
 
     //ステータスを変化させる
-    public void changeStates(int healthValue, int infectionValue, int stressValue)
+    public static void changeStates(int healthValue, int infectionValue, int stressValue)
     {
 
+    }
+
+    public static void ChangeDate()
+    {
+        day += 1;
+    }
+
+    public static void EventStart()
+    {
+        ChangeDate();
+        SceneManager.LoadScene("Natsumatsuri");
     }
 }
